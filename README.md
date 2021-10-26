3ff030a85a1970822828a69b3ed885023ee17931
Kevin Gross <kevin.gross.business@gmail.com>
2021-10-26 16:37:16 +0200

    Prototype

---
+++ README.md
@@ -0,0 +1,11 @@
+# GhostHunt
+
+People are intruding your home and you are a ghost!
+Scare them away by possessing objects or make scary sounds with them.
+
+WASD - Move around
+Space - Move up
+Shift - Move down
+E - Use objects
+
+Developed with Unreal Engine 4
---
+++ Source/GhostHunt/GhostHuntGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GhostHuntGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GHOSTHUNT_API AGhostHuntGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
